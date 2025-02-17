76563bb83ac69ab67d586823a2445efb9fcd0a75
Tony Wu <wuzhentony@gmail.com>
2025-02-16 18:26:11 -0800

    added map display to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
 import ImageDisplay from './ImageDisplay';
+import MapDisplay from './mapDisplay';
 import { useState } from 'react';
 
 function App() {
-  const [coordinates, setCoordinates] = useState([]);
-  
-  if(coordinates.length > 0){
-    console.log(coordinates);
-  }
-  
+  const [coordinates, setCoordinates] = useState([]);  
 
   return (
     <div className="App">
       <header className="App-header">
         <ImageDisplay setCoordinates={setCoordinates}/>
+        <MapDisplay coordinates={coordinates}/>
       </header>
     </div>
   );
