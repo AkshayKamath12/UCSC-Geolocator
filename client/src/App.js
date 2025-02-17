41653ef74b0e646fa9ce5ac6321ef4b68bef50f3
AndreAledia <aaledia@ucsc.edu>
2025-02-16 22:03:33 -0800

    New UI - Semi Functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
+import React, { useState } from 'react';
 import ImageDisplay from './ImageDisplay';
-import { useState } from 'react';
+import BuildingBlocksContent from './BuildingBlocksContent/BuildingBlocksContent';
+import HeroActions from './HeroActions';
 
 function App() {
   const [coordinates, setCoordinates] = useState([]);
-  
-  if(coordinates.length > 0){
+
+  if (coordinates.length > 0) {
     console.log(coordinates);
   }
-  
 
   return (
     <div className="App">
       <header className="App-header">
-        <ImageDisplay setCoordinates={setCoordinates}/>
+        <BuildingBlocksContent />
+        <HeroActions
+          buttonGroupAlign="center"
+          buttonGroupButtonClassName="button1"
+          buttonGroupButtonClassNameOverride="button2"
+          buttonGroupText="Paste from clipboard"
+          platform="desktop"
+          textContentTitleSubtitle="or click the button"
+          textContentTitleTitle="Drag an image here"
+        />
+        <ImageDisplay setCoordinates={setCoordinates} />
       </header>
     </div>
   );
