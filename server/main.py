903c75c094f15befe40a2bbea7ac36d02c153b4b
Akshay Kamath <akkamath@ucsc.edu>
2025-02-24 15:08:11 -0800

    updating server with proper flask endpoint for redis and fixing importing conflicts

--- server/main.py
+++ server/main.py
@@ -3,17 +3,23 @@ from flask_cors import CORS
 import os
 import tempfile
 import tensorflow as tf
-from tensorflow import keras
 import numpy as np
+from redisData.getNearby import set_data, find_closest_coordinates, get_data_from_redis
 
+#set up app and cors to allow requests from node server in front end
 app = Flask(__name__)
 CORS(app)
-allowed_extensions = {"png", "jpg"}
-min_lat = 36.999301
-max_lat = 37.000198
-min_lon = -122.064474
-max_lon = -122.061568
 
+
+#CONSTANTS:
+ALLOWED_EXTENSIONS = {"png", "jpg"}
+MIN_LAT = 36.999301
+MAX_LAT = 37.000198
+MIN_LON = -122.064474
+MAX_LON = -122.061568
+COORDINATES = set_data()
+
+#upload an image and receive predicted coordinates
 @app.route('/upload', methods=['POST'])
 def upload_image():
     
@@ -27,13 +33,13 @@ def upload_image():
         new_filename = generate_temp_upload_filename(filename, "." + extension)  
         print(new_filename)
         file.save(os.path.join('images/upload', new_filename))
-        model = keras.models.load_model("geolocator.keras")
+        model = tf.keras.models.load_model("geolocator.keras")
 
         image = tf.keras.utils.load_img(new_filename, target_size=(224, 224))
         image_array = tf.keras.utils.img_to_array(image) / 255.0
         image_array = tf.expand_dims(image_array, axis=0)
         predictions = model.predict(image_array)
-        predictions = denormalize_predicted_coordinates(predictions, min_lat, max_lat, min_lon, max_lon)[0]
+        predictions = denormalize_predicted_coordinates(predictions, MIN_LAT, MAX_LAT, MIN_LON, MAX_LON)[0]
         os.remove(new_filename)
         print(predictions)
         return jsonify({'prediction': predictions.tolist()}), 200
@@ -42,7 +48,7 @@ def upload_image():
     
 def allowed(file_name):
     res = file_name.split('.')
-    return True if len(res) == 2 and res[1] in allowed_extensions else False
+    return True if len(res) == 2 and res[1] in ALLOWED_EXTENSIONS else False
 
 def generate_temp_upload_filename(filename, extension):
     tmp_file = tempfile.NamedTemporaryFile(delete=False, dir="images/upload", prefix=filename, suffix=extension)
@@ -53,13 +59,17 @@ def denormalize_predicted_coordinates(coords, min_latitude, max_latitude, min_lo
     lon = coords[:, 1] * (max_longitude - min_longitude) + min_longitude
     return np.stack([lat, lon], axis=1)
 
-@app.route('/getNearbyLocationData', methods=['GET'])
+#
+@app.route('/getNearbyLocationData', methods=['POST'])
 def getNearbyLocationData():
     data = request.get_json()
-    if data['coordinates']:
-        [latitude, longitude] = data['coordinates']
-        nearest_coordinates = get_nearest_coordinates(latitude, longitude)
-        return jsonify({'res': nearest_coordinates})
+    if data:
+        coordinate = tuple(data)
+        #note: change coordinates requested below to be a constant at top of file
+        nearest_coordinates = find_closest_coordinates(coordinate, COORDINATES, coordinates_requested=2)
+        res = [[elem[1], get_data_from_redis(elem[1])] for elem in nearest_coordinates]
+        print(res)
+        return jsonify({'res': res})
     else:
         return jsonify({'res': []})
     
