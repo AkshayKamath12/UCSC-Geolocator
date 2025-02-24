81f81958aa0cab36a5ba6b1a82e1e5f87b1e1057
Akshay Kamath <akkamath@ucsc.edu>
2025-02-24 15:06:52 -0800

    sending post request with coordinate to new flask endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,31 @@ import { useState } from 'react';
 function App() {
   const [coordinates, setCoordinates] = useState([]);  
 
+  if(coordinates.length > 0){
+    console.log("sending data")
+    const getData = async () => {
+      try {
+        await fetch('/getNearbyLocationData', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(coordinates)
+        }).then(
+          res =>res.json()
+        ).then(
+          data =>{
+            console.log(data["res"])
+          }
+        )
+      } catch (error) {
+        console.error('Error sending coordinates:', error);
+      }
+    };
+
+    getData()
+  }
+
   return (
     <div className="App">
       <header className="App-header">
