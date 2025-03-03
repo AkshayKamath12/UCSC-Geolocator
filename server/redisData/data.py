34ed1dd7e5987cb95044471787dccdd24c384879
ecerrato <ecerrato@ucsc.edu>
2025-03-02 16:16:35 -0800

    Updated data.py with descriptions to most of them.

--- server/redisData/data.py
+++ server/redisData/data.py
@@ -2,416 +2,416 @@ json_data = [
     {
         'coordinate': (37.00168, -122.05881),
         'name': "Social Sciences 2",
-        'description': "academic building for psychology classes and research"
+        'description': "Academic building for psychology classes and research"
     },
 
     {
         'coordinate': (37.00100, -122.05773),
         'name': 'College 9/10 Dining Hall',
-        'description': "dining hall with a cafe/restaurant upstairs"
+        'description': "Dining hall with a cafe/restaurant upstairs"
     },
 
     {
         'coordinate': (37.00241, -122.05719),
         'name': "Social Sciences 1",
-        'description': "academic building for archaeology classes and research"
+        'description': "Academic building for archaeology classes and research"
     },
 
     {
         'coordinate': (37.00038, -122.05795),
         'name': "Terry Freitas Commons",
-        'description': ""
+        'description': "Terry Freitas Commons (TFC) is a fully student-led organization that serves as a lounge and food pantry for all students."
     },
 
     {
         'coordinate': (37.00076, -122.05719),
         'name': "Namaste Lounge",
-        'description': ""
+        'description': "A student lounge can be used for student organizations events."
     },
 
     {
         'coordinate': (37.00226, -122.05935),
         'name': "College 9/10 Community Room",
-        'description': ""
+        'description': "Community room for college 9/10 students"
     },
 
     {
         'coordinate': (36.99957, -122.05763),
         'name': "History Department",
-        'description': ""
+        'description': "The History Department faculty are a diverse group of scholars whose research and teaching interests often span the divides between traditional academic disciplines."
     },
 
     {
         'coordinate': (36.99969, -122.05759),
         'name': "Department of Technology and Information Management",
-        'description': ""
+        'description': "The Department of Technology and Information Management (TIM) is a unique department in the Baskin School of Engineering that focuses on the management of technology and information."
     },
 
     {
         'coordinate': (36.99964, -122.05767),
         'name': "Granary Child Development Center",
-        'description': ""
+        'description': "A child care for childern of UCSC students and staff"
     },
 
     {
         'coordinate': (37.00071, -122.05853),
         'name': "College 10 Res Halls",
-        'description': ""
+        'description': "Student housing for college 10 students."
     },
 
     {
         'coordinate': (37.00099, -122.05722),
         'name': "College 9 Res Halls",
-        'description': ""
+        'description': "Student housing for college 9 students."
     },
 
     {
         'coordinate': (36.99967, -122.06187),
         'name': "Physical Sciences Building",
-        'description': ""
+        'description': "Contains labs and classrooms for physical science classes.Also, has the department of chemistry and biochemistry and Perk Coffee bar." 
     },
 
     {
         'coordinate': (36.99915, -122.06074),
         'name': "Science and Engineering Library",
-        'description': ""
+        'description': "Univeristy Library with study rooms,video gaming lab,Digital Scholarship Innovation Studio, and East Asian Collection  "
     },
 
     {
         'coordinate': (36.99882, -122.06166),
         'name': "Sinsheimer Labs",
-        'description': ""
+        'description': "A lab that also an academic department for the department of molecular, cell, and developmental biology."
     },
 
     {
         'coordinate': (36.99816, -122.06208),
         'name': "Thimann Labs",
-        'description': ""
+        'description': "A lab that houses the electronics lab, wet lab, meeting spaces, rapid prototyping workshop, machine shop, classrooms, and rooftop greenhouse"
     },
 
     {
         'coordinate': (36.99809, -122.06133),
         'name': "Thimann Lecture Halls",
-        'description': ""
+        'description': "Lecture for classes"
     },
 
     {
         'coordinate': (36.99856, -122.06045),
         'name': "Natural Science Building  2",
-        'description': ""
+        'description': "This building contians classrooms for students and has Kenneth S. Norris Center for Natural History."
     },
 
     {
         'coordinate': (36.99863, -122.05978),
         'name': "Interdisciplinary Sciences",
-        'description': ""
+        'description': "This buidling has the department of Astronomy and Astrophysics Office,department of physics,and Observatories.  "
     },
 
     {
         'coordinate': (36.99802, -122.05962),
         'name': "Earth and Marine Sciences",
-        'description': ""
+        'description': "Building has the department of Earth and Planetary Sciences,Perk Coffee Bar,an auditorium,and Institute of Marine Sciences "
     },
 
     {
         'coordinate': (36.99821, -122.06051),
         'name': "Center for Adaptive Optics",
-        'description': ""
+        'description': "CfAO , for short,mission is to advance and disseminate the technology of adaptive optics (AO) in service to science, health care, industry, and education. Also, other worlds Labortory,(OWL), who are a research and collaboration initiative that focuses on the study of exoplanets."
     },
 
     {
         'coordinate': (36.99706, -122.06203),
         'name': "Kerr Hall",
-        'description': ""
+        'description': "A concrete structure, currently used as the primary administration building on campus."
     },
 
     {
         'coordinate': (36.99580, -122.05878),
         'name': "McHenry Library",
-        'description': ""
+        'description': "University Library with study rooms,global village cafe,Digital Scholarship Innovation Studio, speical collections, and the Grateful Dead Archive."
     },
 
     {
         'coordinate': (36.99520, -122.06163),
         'name': "Media Theater",
-        'description': ""
+        'description': "Performing arts theater and lecture hall."
     },
 
     {
         'coordinate': (36.99449, -122.06093),
-        'name': "Eduardo Carrillo Gallery at UCSC",
-        'description': ""
+        'name': "Elena Baskin Visual Arts complex",
+        'description': "Academic building that has Department of Art,Eduardo Carrillo Gallery,Baskin Visual Arts Courtyard,and Baskin Drawing Studio."
     },
 
     {
         'coordinate': (36.99388, -122.06055),
         'name': "Digital Arts and New Media",
-        'description': ""
+        'description': "Academic department with a research center for digital arts and a solitary garden."
     },
 
     {
         'coordinate': (36.99437, -122.05942),
         'name': "Academic Resources Center",
-        'description': ""
+        'description': "A support service for EOP students and LSS students."
     },
 
     {
         'coordinate': (36.99344, -122.05998),
         'name': "Music Classrooms",
-        'description': ""
+        'description': "UCSC's Music center that contain practice rooms,teaching studios,faculty offices, department offics, and performace studio. "
     },
 
     {
         'coordinate': (36.99307, -122.06072),
         'name': "Recital Hall",
-        'description': ""
+        'description': "An event venue thatbe tuned to customize acoustics for different styles of music and located in the same buidling with the Music department. "
     },
 
 
     {
         'coordinate': (37.00102, -122.06291),
         'name': "Engineering Building 2",
-        'description': "Academic department ,offices of professor,and labs"
+        'description': "Academic department with offices of professor,Baskin Engineering Graduate Programs & Advising,department of Computational Media, and labs"
     },
 
     {
         'coordinate': (37.00041, -122.06320),
         'name': "Jack Baskin Engineering",
-        'description': ""
+        'description': "Also known as E1 building, this building has the department of Computer Science and Engineering,advising office, labs, the department of Electrical and Computer Engineering, Perk coffee, and the Baskin Engineering Dean's Office."
     },
 
     {
         'coordinate': (37.00085, -122.06146),
         'name': "Communications",
-        'description': ""
+        'description': "Department of Film and Digital Media and the department of Communication."
     },
 
     {
         'coordinate': (37.00153, -122.06166),
         'name': "Natural Sciences Greenhouse",
-        'description': ""
+        'description': "Greenhouse for the department of Ecology and Evolutionary Biology."
     },
 
     {
         'coordinate': (37.00164, -122.06176),
         'name': "Environmental Health and Safety",
-        'description': ""
+        'description': "EHS monitors compliance with environmental, health and safety programs intended to minimize or prevent occupational injuries and illnesses in the workplace and to protect the quality of the surrounding environment."
     },
 
     {
         'coordinate': (37.00152, -122.06133),
         'name': "Observatories",
-        'description': ""
+        'description': "UCO is a multi-campus research unit that conduct astronomical research. "
     },
 
     {
         'coordinate': (37.00021, -122.06235),
         'name': "Jack Baskin Engineering Auditorium",
-        'description': ""
+        'description': "Auditorium for lectures and events."
     },
 
     {
         'coordinate': (36.99259, -122.06476),
         'name': "Visual Arts Research Facility",
-        'description': ""
+        'description': "Academic department that Supports students, faculty, and graduate students in creative research, scholarship, and public engagement. "
     },
 
     {
         'coordinate': (36.99164, -122.06539),
         'name': "Rachel Carson and Oakes Dining Hall",
-        'description': ""
+        'description': "Dining hall for Rachel Carson and Oakes College students."
     },
 
     {
         'coordinate': (36.99180, -122.06454),
         'name': "Rachel Carson Academic Building",
-        'description': ""
+        'description': "Academic building with classrooms and labs."
     },
 
     {
         'coordinate': (36.99140, -122.06407),
         'name': "Athletics and Recreation West Field House",
-        'description': ""
+        'description': "Gym and fitness center."
     },
 
     {
         'coordinate': (36.99125, -122.06480),
         'name': "Red Room",
-        'description': ""
+        'description': "A student study lounge and can be booked for use by student organizations for meetings or small events"
     },
 
     {
         'coordinate': (36.98962, -122.06281),
         'name': "Oakes Academic Buildings",
-        'description': ""
+        'description': "Helds the department of Community Studies and classrooms."
     },
 
     {
         'coordinate': (36.99007, -122.06284),
         'name': "Department of Community Studies",
-        'description': ""
+        'description': "Academic department that focuses on social justice and community engagement."
     },
 
     {
         'coordinate': (36.98982, -122.06282),
         'name': "Oakes Library",
-        'description': ""
+        'description': "Library for Oakes College students."
     },
 
     {
         'coordinate': (36.98952, -122.06277),
         'name': "Westside Writing Center",
-        'description': ""
+        'description': "The Westside/Eastside Writing Center offers writing consultation services and tutoring designed to teach and support students in all aspects and phases of scholastic writing."
     },
 
     {
         'coordinate': (36.98925, -122.06330),
         'name': "Oakes Cafe",
-        'description': ""
+        'description': "Located at Oakes College, Oakes Cafe offers breakfast, lunch, and dinner. "
     },
 
     {
         'coordinate': (36.98889, -122.06273),
         'name': "Oakes Garden",
-        'description': ""
+        'description': "A community garden for Oakes College students."
     },
 
     {
         'coordinate': (36.98859, -122.06285),
         'name': "Eric Thomas Memorial Bench",
-        'description': ""
+        'description': "A Historical landmark."
     },
 
     {
         'coordinate': (36.98859, -122.06590),
         'name': "West Remote Parking Lot",
-        'description': ""
+        'description': "Parking lot for students."
     },
 
     {
         'coordinate': (36.99252, -122.06537),
         'name': "West Tennis Courts",
-        'description': ""
+        'description': "Tennis courts for students."
     },
 
     {
         'coordinate': (36.99197, -122.06577),
         'name': "Volleyball Court",
-        'description': ""
+        'description': "Volleyball courts for students."
     },
 
     {
         'coordinate': (36.99142, -122.06590),
         'name': "Rachel Carson Garden",
-        'description': ""
+        'description': "Community garden for Rachel Carson College students."
     },
 
     {
         'coordinate': (36.98801, -122.06566),
         'name': "Oakes Meadow",
-        'description': ""
+        'description': "Park."
     },
 
     {
         'coordinate': (36.98981, -122.06575),
         'name': "FSH Food Distribution Center",
-        'description': ""
+        'description': "Food bank for famiy student housing residents students that features produce from the UCSC Farm."
     },
 
     {
         'coordinate': (37.00009, -122.05329),
         'name': "Baobab Lounge",
-        'description': ""
+        'description': "Lounge for UCSC Merrill students."
     },
 
     {
         'coordinate': (36.99971, -122.05363),
         'name': "Merrill Academic",
-        'description': ""
+        'description': "Merrill Academic Building that contains office of professors and classrooms."
     },
 
     {
         'coordinate': (37.00006, -122.05384),
         'name': "Merrill Cultural Center",
-        'description': ""
+        'description': "Event center that is designed to hold lectures, dinners, theater performances, and other large-scale events."
     },
 
     {
         'coordinate': (37.00013, -122.05437),
         'name': "Crown and Merrill Dining Hall",
-        'description': ""
+        'description': "Dining hall for Crown and Merrill College students."
     },
 
     {
         'coordinate': (37.00003, -122.05480),
         'name': "Merrill Classroom Building",
-        'description': ""
+        'description': "Building contains classrooms."
     },
 
     {
         'coordinate': (36.99984, -122.05481),
         'name': "Crown Library",
-        'description': ""
+        'description': "Library for Crown College students."
     },
 
     {
         'coordinate': (36.99948, -122.05368),
         'name': "Merrill Lounge",
-        'description': ""
+        'description': "Student lounge for Merrill College students."
     },
 
     {
         'coordinate': (36.99940, -122.05356),
         'name': "Merrill Campus Advising",
-        'description': ""
+        'description': "Faculty and staff offices for Merrill College."
     },
 
     {
         'coordinate': (36.99937, -122.05331),
         'name': "Casa Latina",
-        'description': ""
+        'description': "Casa Latina is a quiet study area for students"
     },
 
     {
         'coordinate': (36.99971, -122.05227),
         'name': "Ming Ong Computer Center",
-        'description': ""
+        'description': "The Ming Ong Computer Center at Merrill College celebrates the life of Juei-Ming Ong. Open 24 hours equipped with  PC computers, printers, 2 whiteboard and scanners."
     },
 
     {
         'coordinate': (36.99694, -122.05502),
         'name': "Cowell Computer Lab",
-        'description': ""
+        'description': "Computer lab that is open 24 hours and equipped with Mac and PC computers, printers, and scanners."
     },
 
     {
         'coordinate': (36.99841, -122.05450),
         'name': "Humanities Lecture Hall",
-        'description': ""
+        'description': "Lecture hall forclasses."
     },
 
     {
         'coordinate': (36.99840, -122.05489),
         'name': "Humanities and Social Sciences",
-        'description': ""
+        'description': "Academic department that  house faculty offices and classrooms to serve the Division of Humanities and the Education Department of the Division of Social Sciences."
     },
 
     {
         'coordinate': (36.99805, -122.05457),
         'name': "Humanities 1",
-        'description': ""
+        'description': "Academic building that contains classrooms,faculty offices,Department of History of Consciousness, and the Dickens Project ."
     },
 
     {
         'coordinate': (36.99737, -122.05371),
         'name': "Cowell Classrooms",
-        'description': ""
+        'description': "Building contains classrooms , the Department of Philosophy,Cowell Conference Room,and Cowell College Administration."
     },
 
     {
         'coordinate': (36.99735, -122.05310),
         'name': "Classics Library",
-        'description': ""
+        'description': "Education center that The library contains books in the classics, and there is also a dedicated student librarian. "
     },
 
     {
@@ -428,7 +428,7 @@ json_data = [
 
     {
         'coordinate': (36.99725, -122.05169),
-        'name': "Stevnson Classrooms",
+        'name': "Stevenson Classrooms",
         'description': ""
     },
 
@@ -608,5 +608,12 @@ json_data = [
         'name': "Merrill Market",
         'description': "Convenience store."
     },
+
+{
+        'coordinate': (36.99975 , -122.05746),
+        'name': "Cowell Student Health Center",
+        'description': "Medical clinic for UCSC student that has with a pharmacy,SHOP services,counseling, psychological services, and more"
+    },
+
 
 ]
